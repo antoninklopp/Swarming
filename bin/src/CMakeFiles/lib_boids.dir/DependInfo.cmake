87bde071d32121fbf296cbff9249b2bc0a4e1bc0
Antonin KLOPP-TOSSER <antonin.klopp-tosser@grenoble-inp.org>
2019-01-28 19:20:55 +0100

    Transfered to CmakeLists architecture

---
+++ bin/src/CMakeFiles/lib_boids.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/user/2/.base/klopptoa/home/Documents/3A/Swarming/src/agent.cxx" "/user/2/.base/klopptoa/home/Documents/3A/Swarming/bin/src/CMakeFiles/lib_boids.dir/agent.cxx.o"
+  "/user/2/.base/klopptoa/home/Documents/3A/Swarming/src/vector.cxx" "/user/2/.base/klopptoa/home/Documents/3A/Swarming/bin/src/CMakeFiles/lib_boids.dir/vector.cxx.o"
+  "/user/2/.base/klopptoa/home/Documents/3A/Swarming/src/workspace.cxx" "/user/2/.base/klopptoa/home/Documents/3A/Swarming/bin/src/CMakeFiles/lib_boids.dir/workspace.cxx.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../src/library"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ bin/src/CMakeFiles/lib_boids.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/lib_boids.dir/agent.cxx.o"
+  "CMakeFiles/lib_boids.dir/vector.cxx.o"
+  "CMakeFiles/lib_boids.dir/workspace.cxx.o"
+  "liblib_boids.pdb"
+  "liblib_boids.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/lib_boids.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
